5279ead8c7ec5581bcc9eb41d4da775f73a15307
Jose Ramon Figueira <jose_ramon_figueira_alvarez@carrefour.com>
2020-03-10 03:48:30 +0100

    Creation tests

---
+++ src/test/java/code/challenge/jrfigueira/stepdefs/StepDefsIntegrationTest.java
@@ -0,0 +1,37 @@
+package code.challenge.jrfigueira.stepdefs;
+
+import code.challenge.jrfigueira.SpringIntegrationTest;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class StepDefsIntegrationTest extends SpringIntegrationTest {
+
+    @When("^the client calls /baeldung$")
+    public void the_client_issues_POST_hello() throws Throwable {
+        //executePost();
+    }
+
+    @Given("^the client calls /hello$")
+    public void the_client_issues_GET_hello() throws Throwable {
+        //executeGet("http://localhost:8082/hello");
+    }
+
+    @When("^the client calls /version$")
+    public void the_client_issues_GET_version() throws Throwable {
+        System.out.println("BIEEEEEEEEEEEEEEEEEEEN");
+        //executeGet("http://localhost:8082/version");
+    }
+
+    @Then("^the client receives status code of (\\d+)$")
+    public void the_client_receives_status_code_of(int statusCode) throws Throwable {
+        //final HttpStatus currentStatusCode = latestResponse.getTheResponse().getStatusCode();
+        //assertThat("status code is incorrect : " + latestResponse.getBody(), currentStatusCode.value(), is(statusCode));
+    }
+
+    @And("^the client receives server version (.+)$")
+    public void the_client_receives_server_version_body(String version) throws Throwable {
+        //assertThat(latestResponse.getBody(), is(version));
+    }
+}
